71d95f2b35e2b7ee0e24e60d9f46beb9546513cc
Bekirhan Kurt <bekirhankurtis@gmail.com>
2025-03-25 16:08:24 +0300

    CrossCuttingConcerns implemented

---
+++ CrossCuttingConcerns/Logging/Serilog/Logger/FileLogger.cs
@@ -0,0 +1,23 @@
+﻿using CrossCuttingConcerns.Logging.Serilog.ConfigurationModels;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using static CrossCuttingConcerns.Logging.Serilog.Messages.SerilogMessages;
+
+namespace CrossCuttingConcerns.Logging.Serilog.Logger;
+
+public class FileLogger : LoggerServiceBase
+{
+    public FileLogger(IConfiguration configuration)
+    {
+        var logConfig =
+            configuration.GetSection("SerilogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>() ??
+            throw new ArgumentNullException(NullOptionsMessage);
+
+        var logFilePath = string.Format("{0},{1}", Directory.GetCurrentDirectory() + logConfig.FolderPath, ".txt");
+
+        Logger = new LoggerConfiguration().WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day,
+                retainedFileTimeLimit: null, fileSizeLimitBytes: 5000000,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}")
+            .CreateLogger();
+    }
+}
---
+++ CrossCuttingConcerns/Logging/Serilog/Messages/SerilogMessages.cs
@@ -0,0 +1,6 @@
+﻿namespace CrossCuttingConcerns.Logging.Serilog.Messages;
+
+public static class SerilogMessages
+{
+    public static string NullOptionsMessage => "Received a blank value. Something went terribly wrong.";
+}
